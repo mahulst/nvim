a1d3745f333e2dedac6b314abf1b2c374271b0bd
mahulst <michel@voorkanter.com>
2023-10-14 13:19:01 +0200

    Adds obsidian config

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,37 +1,81 @@
--- since this is just an example spec, don't actually load anything here and return an empty spec
--- stylua: ignore
-return { 
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        kotlin_language_server = {},
-      },
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                kotlin_language_server = {},
+            },
+        },
     },
-  },
-  {
-    "ThePrimeagen/harpoon",
-    lazy = false,
-    dependencies = {
-      "nvim-lua/plenary.nvim",
+    {
+        "ThePrimeagen/harpoon",
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        config = true,
+        keys = {
+            {
+                "<leader>ha",
+                function()
+                    require("harpoon.mark").add_file()
+                end,
+                desc = "Mark file with harpoon",
+            },
+            {
+                "<ileader>hn",
+                function()
+                    require("harpoon.ui").nav_next()
+                end,
+                desc = "Go to next harpoon mark",
+            },
+            {
+                "<leader>hp",
+                function()
+                    require("harpoon.ui").nav_prev()
+                end,
+                desc = "Go to previous harpoon mark",
+            },
+            {
+                "<leader>hy",
+                function()
+                    require("harpoon.ui").nav_file(1)
+                end,
+                desc = "Go to harpoon 1",
+            },
+            {
+                "<leader>hu",
+                function()
+                    require("harpoon.ui").nav_file(2)
+                end,
+                desc = "Go to harpoon 2",
+            },
+            {
+                "<leader>hi",
+                function()
+                    require("harpoon.ui").nav_file(3)
+                end,
+                desc = "Go to harpoon 3",
+            },
+            {
+                "<leader>ho",
+                function()
+                    require("harpoon.ui").nav_file(4)
+                end,
+                desc = "Go to harpoon 4",
+            },
+            {
+                "<leader>hm",
+                function()
+                    require("harpoon.ui").toggle_quick_menu()
+                end,
+                desc = "Show harpoon marks",
+            },
+        },
     },
-    config = true,
-    keys = {
-      { "<leader>ha", function() require('harpoon.mark').add_file() end, desc = "Mark file with harpoon" },
-      { "<ileader>hn", function () require('harpoon.ui').nav_next() end, desc = "Go to next harpoon mark" },
-      { "<leader>hp", function () require('harpoon.ui').nav_prev() end, desc = "Go to previous harpoon mark" },
-      { "<leader>hy", function () require('harpoon.ui').nav_file(1) end, desc = "Go to harpoon 1" },
-      { "<leader>hu", function () require('harpoon.ui').nav_file(2) end, desc = "Go to harpoon 2" },
-      { "<leader>hi", function () require('harpoon.ui').nav_file(3) end, desc = "Go to harpoon 3" },
-      { "<leader>ho", function () require('harpoon.ui').nav_file(4) end, desc = "Go to harpoon 4" },
-      { "<leader>hm", function () require('harpoon.ui').toggle_quick_menu() end, desc = "Show harpoon marks" },
-    }
-  },
-  {
-    "nvim-telescope/telescope.nvim",
-    keys = {
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
       -- add a keymap to browse plugin files
       -- stylua: ignore
       {
@@ -39,17 +83,31 @@ return {
         function() require("telescope.builtin").git_files() end,
         desc = "Find files in git",
       },
+        },
+        -- change some options
     },
-    -- change some options
-  },
-  { "tpope/vim-fugitive" },
-  {
-    "mbbill/undotree",
-    keys = {
-      { "<leader>ut", 
-        ":UndotreeShow<CR>",
-        desc = "Toogle undo tree"
-      }
-    }
-  }
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        "mbbill/undotree",
+        keys = {
+            { "<leader>ut", ":UndotreeShow<CR>", desc = "Toogle undo tree" },
+        },
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        lazy = true,
+        event = {
+            "BufReadPre  /home/mahulst/obsidian/vault/**.md",
+            "BufNewFile /home/mahulst/obsidian/vault/**.md",
+        },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            dir = "~/obsidian/vault",
+            mappings = {},
+        },
+    },
 }
