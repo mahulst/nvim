d3bdcfcee3aeb9d7fefa299b23f062719c878021
mahulst <michel@voorkanter.com>
2023-10-16 10:14:34 +0200

    Adds keymaps for navigating

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,19 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.del("n", "<leader>gg")
 vim.keymap.del("n", "<leader>gG")
+
+vim.keymap.set("n", "J", "mzJ`z")
+-- Keep cursor in middle while jumping
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- Keep cursor in middle while searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Paste over while keeping your copied value
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
