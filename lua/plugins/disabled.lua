14a8098819df4668595303187d83324b87e0ac10
mahulst <michel@voorkanter.com>
2023-10-20 21:14:07 +0200

    Adds hardtime and disable nvim popups

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
     { "nvim-neo-tree/neo-tree.nvim", enabled = false },
     { "folke/noice.nvim", enabled = false },
     { "akinsho/bufferline.nvim", enabled = false },
+    { "rcarriga/nvim-notify", enabled = false },
 }
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -161,4 +161,9 @@ return {
             })
         end,
     },
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
 }
